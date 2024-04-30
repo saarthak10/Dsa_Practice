736982b57354d7a65d8edbceb5b135b20fd9ca2f
saarthak10 <saarthaksharma87@gmail.com>
2024-05-01 00:52:21 +0530

    pattern number six and seven completed day 2

---
+++ Patterns/Pattern6.java
@@ -0,0 +1,21 @@
+package Patterns;
+public class Pattern6 {
+     /*
+     *  12345
+     *  1234
+     *  123
+     *  12 
+     *  1
+     */
+    public static void printPattern(){
+        for(int  i= 5; i>0; i--){
+            for(int j = 1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        printPattern();
+    }
+}
