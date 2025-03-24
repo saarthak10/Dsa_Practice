54199f5383e69f76d5c27a83f66f4a13ca224b40
saarthak10 <saarthaksharma87@gmail.com>
2025-03-24 15:05:44 +0530

    square pattern added

---
+++ Patterns/SquarePattern.java
@@ -0,0 +1,29 @@
+package Patterns;
+
+public class SquarePattern {
+    
+
+    public static void printSquare(int n){
+      
+        for(int i=0; i< n; i++){
+           int valueToPrint = 2*i + 1;
+            
+            for(int j=0; j< n; j++){
+                System.out.print(valueToPrint + " ");
+                valueToPrint = valueToPrint + 2;
+                if(valueToPrint > 2*n -1){
+                    valueToPrint = 1;
+                    
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+    
+
+
+public static void main(String[] args) {
+    printSquare(5);
+}
+}
