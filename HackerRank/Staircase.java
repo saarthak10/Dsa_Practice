e7048624afcbd13a10783dc09ebc7fe19e6f18f6
saarthak10 <saarthaksharma87@gmail.com>
2025-04-10 20:35:34 +0530

    staircase problem started

---
+++ HackerRank/Staircase.java
@@ -0,0 +1,22 @@
+package HackerRank;
+
+public class Staircase {
+
+   public static void printStaircase(int n){
+
+    for(int i=0; i< n; i++){
+        for(int j=i; j< n-i -1; j++){
+            System.out.println(" ");
+        }
+
+    }
+
+   }
+   
+   
+    public static void main(String[] args){
+
+
+
+    }
+}
