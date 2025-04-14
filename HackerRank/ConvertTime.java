fef5a3bef3f1ffdfeed0eac2053e7cc031bb602e
saarthak10 <saarthaksharma87@gmail.com>
2025-04-15 03:04:27 +0530

    9th problem for hackerrank done

--- HackerRank/ConvertTime.java
+++ HackerRank/ConvertTime.java
@@ -2,7 +2,36 @@ package HackerRank;
 
 public class ConvertTime {
     
+    public static String convertTime(String s){
+        String result = "";
+        if(s.charAt(8)=='P'){
+            System.out.println("I AM HERE===>");
+            String portion=s.substring(0,8);
+            String[] time=portion.split(":");
+            if(time[0].matches("12")){
+                System.out.println(time[0]+":"+time[1]+":"+time[2]);
+                result = time[0]+":"+time[1]+":"+time[2];
+            }else{
+                time[0]=String.valueOf(Integer.valueOf(time[0])+12);
+                System.out.println(time[0]+":"+time[1]+":"+time[2]);
+                result = time[0]+":"+time[1]+":"+time[2];
+            }
+        }else{
+            String portion=s.substring(0,8);
+            String[] time=portion.split(":");
+            if(time[0].matches("12")){
+                time[0]="00";
+                System.out.println(time[0]+":"+time[1]+":"+time[2]);
+                result = time[0]+":"+time[1]+":"+time[2];
+            }else{
+                System.out.println(portion);
+                result = portion;
+            }
+        }
+        return result;
+    }
     public static void main(String[] args) {
-        
+        String time = "12:45:54PM";
+        convertTime(time);
     }
 }
