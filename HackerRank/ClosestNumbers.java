e07f0cf804c979f1950bcc33f4b8a1e45a8349d7
saarthak10 <saarthaksharma87@gmail.com>
2025-05-07 03:30:13 +0530

    closest number problem done

--- HackerRank/ClosestNumbers.java
+++ HackerRank/ClosestNumbers.java
@@ -2,6 +2,7 @@ package HackerRank;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 /*
@@ -14,16 +15,36 @@ public class ClosestNumbers {
 
     public static List<Integer> findClosestNumbers(List<Integer>givenArray){
         List<Integer> result = new ArrayList<>();
+        HashMap<Integer,Integer> differenceMap = new HashMap<>();
         Collections.sort(givenArray);
+        int min_difference = Integer.MAX_VALUE;        
         for(int i=0; i< givenArray.size()-1;i++){
-            int diff = Math.abs(givenArray.get(i) - givenArray.get(i+1));
+            int difference =Math.abs(givenArray.get(i) - givenArray.get(i+1)); 
+            differenceMap.put(i, difference);
+            min_difference = Math.min(min_difference, difference);
             
         }
-        
+
+        for(int key : differenceMap.keySet()){
+            if(differenceMap.get(key) == min_difference){
+                result.add(givenArray.get(key));
+                result.add(givenArray.get(key + 1));
+            }
+        }
+        System.out.println("RESULT IS ===> " + result);
+
         return result;
     }
 
     public static void main(String[] args) {
+        ArrayList<Integer> barArray = new ArrayList<>();
+        barArray.add(-5);
+        barArray.add(15);
+        barArray.add(25);
+        barArray.add(71);
+        barArray.add(63);
+
         
+        findClosestNumbers(barArray);
     }
 }
