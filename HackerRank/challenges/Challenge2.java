7e7f285d3b29fe8935b60cfe3148e75e20746503
saarthak10 <saarthaksharma87@gmail.com>
2025-05-13 20:26:17 +0530

    challenge 2 completed

--- HackerRank/challenges/Challenge2.java
+++ HackerRank/challenges/Challenge2.java
@@ -12,24 +12,33 @@ public class Challenge2 {
     public static void insertionSort1(int n, List<Integer> arr) {
         // Write your code here
         int lastValue = arr.get(n-1);
-        for(int i=n-1;i> 0; i--){
+        for(int i=n-2;i>= 0; i--){
             if(arr.get(i)< lastValue){
-                arr.add(i, lastValue);
-                System.out.println("INSIDE IF" +arr);
+                arr.set(i+1, lastValue);
+                printArray(arr);
+                break;
             }else{
-                arr.add( i-1, arr.get(i));
-                System.out.println("INSIDE ELSE" +arr);
+                arr.set( i +1, arr.get(i));
+                printArray(arr);
 
             }
+            System.out.println();
         }
+    }
+
+    public static void printArray(List<Integer> arr){
+        for(int i=0; i< arr.size(); i++){
+            System.out.print(arr.get(i) + " ");
+        }
+
     }
 
     public static void main(String[] args) {
          ArrayList<Integer> barArray = new ArrayList<>();
-        barArray.add(1);
         barArray.add(2);
         barArray.add(4);
-        barArray.add(5);
+        barArray.add(6);
+        barArray.add(8);
         barArray.add(3);
         insertionSort1(5, barArray);
     }
