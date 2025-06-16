d856833606946fa12f40c96874f5846845b5bda3
saarthak10 <saarthaksharma87@gmail.com>
2025-06-16 13:53:55 +0530

    minimum operations problem efficient solution

--- Hashing/Hashing/MinimumOperationsArray.java
+++ Hashing/Hashing/MinimumOperationsArray.java
@@ -1,5 +1,8 @@
 package Hashing.Hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * Question:- Part 6: Minimum operations to make all elements equal in an array.
  * 
@@ -14,6 +17,7 @@ public class MinimumOperationsArray {
      */
     public static int findMinimumOperationToMakeArrayElementsEqualBrute(int[] givenArray){
         int maxFreq = 0;
+        
         for(int i=0;i< givenArray.length; i++){
             int currentFrequency =1;
             for(int j =0; j< givenArray.length; j++){
@@ -37,13 +41,30 @@ public class MinimumOperationsArray {
      * 2.) Then return minimum operations using arraySize - maxFrequency
      */
     public static int findMinimumOpsEfficient(int[] givenArray){
+        int maxFrequency = 0;
+        HashMap<Integer,Integer> frequencyMap = new HashMap<>();
+        for(int i=0; i< givenArray.length; i++){
+            int currentFrequency = frequencyMap.getOrDefault(givenArray[i], 0);
+            frequencyMap.put(givenArray[i], currentFrequency + 1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry: frequencyMap.entrySet() ){
+            if (entry.getValue() > maxFrequency) {
+                maxFrequency = entry.getValue();
+            }
+
+        }
+        int minimumOperations = givenArray.length - maxFrequency;
+        System.out.println("Minimum Operations Required are " + minimumOperations);
+
         
-        
-        return 0;
+        return minimumOperations;
     }
 
     public static void main(String[] args) {
             int arr[] = {1,2,3,4};
-            findMinimumOperationToMakeArrayElementsEqualBrute(arr);
+            int arr1[] = {1 , 2, 1, 4, 3, 1};
+            findMinimumOperationToMakeArrayElementsEqualBrute(arr1);
+            findMinimumOpsEfficient(arr1);
     }
 }
