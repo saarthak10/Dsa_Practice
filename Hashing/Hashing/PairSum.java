9c35ecbb0daca23b1d2bb8218998af4ac9f41d1c
saarthak10 <saarthaksharma87@gmail.com>
2025-06-17 13:03:17 +0530

    pair sum problme started

---
+++ Hashing/Hashing/PairSum.java
@@ -0,0 +1,19 @@
+package Hashing.Hashing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PairSum {
+    
+    public static List findPair(int targetElement, int[] givenArray){
+        ArrayList<Integer> result = new ArrayList<>();
+        
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
